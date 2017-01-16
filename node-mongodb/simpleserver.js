a7268ef2c4e41b005257cf990390eb29449abf77
kvin1087 <kaustubh87@gmail.com>
2017-01-16 15:59:28 -0600

    Using Mongodb to do operations

---
+++ node-mongodb/simpleserver.js
@@ -0,0 +1,27 @@
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+var url = 'mongodb://localhost:27017/conFusion';
+
+MongoClient.connect(url, function(err, db){
+  assert.equal(err, null);
+
+  var collection = db.collection("dishes");
+  collection.insertOne({name: 'Uthapizza', description: 'test'}, function(err, result){
+    assert.equal(err,null);
+    console.log('Insertion was successful');
+    console.log(result.ops);
+
+  collection.find({}).toArray(function(err, docs){
+    assert.equal(err, null);
+    console.log("Found: ");
+    console.log(docs);
+
+  db.dropCollection("dishes", function(err, result){
+    assert.equal(err, null);
+    console.log('Deleted collection');
+    db.close();
+  });
+  });
+  });
+});
