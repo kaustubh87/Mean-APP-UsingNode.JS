70ad8b96cad263798526edb08e68baf3581057ce
kvin1087 <kaustubh87@gmail.com>
2017-01-15 20:44:38 -0600

    Nodejs server with express routers

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var hostname = 'localhost';
+var port = 4300;
+
+var dishRouter = require('./dishRouter.js');
+var promoRouter = require('./promoRouter.js');
+var leaderRouter = require('./leaderRouter.js');
+
+var app = express();
+app.use(morgan('dev'));
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leadership', leaderRouter);
+
+app.listen(port, hostname, function(){
+  console.log('Server running at ' +hostname + ' and port no ' +port );
+});
