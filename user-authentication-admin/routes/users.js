2a0c9f5eafc69ca64cbeb4085ef12c49939cdf71
kvin1087 <kaustubh87@gmail.com>
2017-02-14 06:02:26 -0600

    Comments populated

--- user-authentication-admin/routes/users.js
+++ user-authentication-admin/routes/users.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var bodyParser = require('body-parser');
 var User = require('../models/user');
 var Verify    = require('./verify');
 
-/* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
+router.use(bodyParser.json());
+
+router.route('/users')
+.get(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req,res,next){
+if(Verify.verifyAdmin){
+  User.find({}, function(err,user){
+    if(err) throw err;
+    res.json(user);
+  });
+}
+else {
+  var err = new Error("You are not authorized to perform this operation");
+  err.status = 403;
+  return next(err);
+}
 });
 
 router.post('/register', function(req, res) {
